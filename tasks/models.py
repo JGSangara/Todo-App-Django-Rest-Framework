c54ebc5c7efdaa3494e910255b83e636bc84d84a
Josphat Gitogo Sangara <josphat.gitogo@gmail.com>
2023-03-20 02:28:33 +0300

    Initial commit

---
+++ tasks/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField(auto_now=False, auto_now_add=False)
+    completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks")
+    shared_with = models.ManyToManyField(User, related_name="shared_tasks")
