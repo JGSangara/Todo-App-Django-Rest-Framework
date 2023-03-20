1d2faba97f8bd31fa87311f29ca4888b5d919099
Josphat Gitogo Sangara <josphat.gitogo@gmail.com>
2023-03-20 04:43:18 +0300

    Setup admin for accounts & tasks app

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -20,13 +21,15 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    username = None
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created', 'due', 'completed', 'owner')
+    list_display_links = ('id', 'title')
+    list_filter = ('completed', 'owner')
+    search_fields = ('title', 'owner__email')
+    list_per_page = 25
