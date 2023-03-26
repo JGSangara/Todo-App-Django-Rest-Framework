9a3965c1990c31a0398de13ef5923bf3ecb21e17
Josphat Gitogo Sangara <josphat.gitogo@gmail.com>
2023-03-26 05:09:31 +0300

    Setup django simple JWT

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, LoginView, RegisterView, ChangePasswordView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
-
+from .views import ChangePasswordView, RegisterView, TaskDetail, TaskList
 
 app_name = 'api'
 
@@ -14,9 +10,10 @@ urlpatterns = [
     path('tasks/', TaskList.as_view(), name='task-list'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
     # authentication
-    path('users/login/', LoginView.as_view(), name='login'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/login/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('users/register/', RegisterView.as_view(), name='register'),
     path('users/change-password/', ChangePasswordView.as_view(), name='change-password'),
-
 
 ]
--- api/views.py
+++ api/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from accounts.models import User
 from tasks.models import Task
 
-from .serializers import (ChangePasswordSerializer, LoginSerializer,
-                          RegisterSerializer, TaskSerializer)
+from .serializers import (ChangePasswordSerializer, RegisterSerializer,
+                          TaskSerializer)
 
 # Create your views here.
 
@@ -19,20 +19,6 @@ class TaskList(generics.ListCreateAPIView):
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-
-class LoginView(generics.GenericAPIView):
-    serializer_class = LoginSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response({
-            "success": True,
-            "status code": status.HTTP_200_OK,
-            "message": "User logged in  successfully",
-            "token": serializer.data['token']
-        }, status=status.HTTP_200_OK)
 
 
 class RegisterView(generics.GenericAPIView):
